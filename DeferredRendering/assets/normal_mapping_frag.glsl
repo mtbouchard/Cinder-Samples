088d116d1a1da81a83302b75a526b80242291270
Paul Houx <paul@cowlumbus.nl>
2013-11-10 01:23:30 +0100

    Normal mapping sample added and renamed to DeferredRendering. I intend to add SSAO and other advanced rendering techniques, to show off what is possible.

---
+++ DeferredRendering/assets/normal_mapping_frag.glsl
@@ -0,0 +1,60 @@
+#version 110
+
+varying vec4		vVertex;
+varying vec3		vNormal;
+varying vec3		vTangent;
+varying vec3		vBiTangent;
+
+uniform	sampler2D	uDiffuseMap;
+uniform	sampler2D	uSpecularMap;
+uniform	sampler2D	uNormalMap;
+
+uniform bool		bShowNormals;
+
+uniform bool		bUseDiffuseMap;
+uniform bool		bUseSpecularMap;
+uniform bool		bUseNormalMap;
+
+void main()
+{
+	// fetch the normal from the normal map and modify it using the normal (and tangents) from the 3D mesh
+	vec3	vMappedNormal = texture2D(uNormalMap, gl_TexCoord[0].st).rgb * 2.0 - 1.0;
+	vec3	vSurfaceNormal = bUseNormalMap ? normalize((vTangent * vMappedNormal.x) + (vBiTangent * vMappedNormal.y) + (vNormal * vMappedNormal.z)) : vNormal;	
+ 
+	vec3	vToCamera = normalize(-vVertex.xyz); 
+
+	// apply each of our light sources
+	vec4	vDiffuseColor	= vec4(0, 0, 0, 1);
+	vec4	vSpecularColor	= vec4(0, 0, 0, 1);
+
+	for(int i=0; i<2; ++i)
+	{
+		// calculate view space light vectors (for directional light source)
+		vec3	vToLight = normalize(-gl_LightSource[i].position.xyz); 
+		vec3	vReflect = normalize(-reflect(vToLight,vSurfaceNormal));
+
+		// calculate diffuse term
+		float	fDiffuse = max(dot(vSurfaceNormal,vToLight), 0.0);
+		fDiffuse = clamp(fDiffuse, 0.1, 1.0);  
+
+		// calculate specular term
+		float	fSpecularPower = 100.0;
+		float	fSpecular = pow( max( dot(vReflect, vToCamera), 0.0), fSpecularPower );
+		fSpecular = clamp(fSpecular, 0.0, 1.0);
+
+		// calculate final colors
+		if(bUseDiffuseMap)
+			vDiffuseColor += texture2D(uDiffuseMap, gl_TexCoord[0].st) * gl_LightSource[i].diffuse * fDiffuse;
+		else
+			vDiffuseColor += gl_LightSource[i].diffuse * fDiffuse; 
+
+		if(bUseSpecularMap)
+			vSpecularColor += texture2D(uSpecularMap, gl_TexCoord[0].st) * gl_LightSource[i].specular * fSpecular;
+		else
+			vSpecularColor += gl_LightSource[i].specular * fSpecular; 
+	}
+
+	// output colors to buffer
+	gl_FragColor.rgb = bShowNormals ? vSurfaceNormal : (vDiffuseColor + vSpecularColor).rgb;
+	gl_FragColor.a = 1.0;
+}
---
+++ DeferredRendering/src/DeferredRenderingApp.cpp
@@ -0,0 +1,404 @@
+/*
+ Copyright (c) 2013, Paul Houx - All rights reserved.
+ This code is intended for use with the Cinder C++ library: http://libcinder.org
+
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and
+	the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+	the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "cinder/app/AppNative.h"
+#include "cinder/gl/gl.h"
+#include "cinder/gl/GlslProg.h"
+#include "cinder/gl/Light.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/gl/Vbo.h"
+#include "cinder/params/Params.h"
+#include "cinder/Camera.h"
+#include "cinder/ImageIo.h"
+#include "cinder/MayaCamUI.h"
+#include "cinder/ObjLoader.h"
+#include "cinder/Timer.h"
+#include "cinder/TriMesh.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class DeferredRenderingApp : public AppNative {
+public:
+	void	prepareSettings( Settings* settings );
+
+	void	setup();
+	void	shutdown();
+
+	void	update();
+	void	draw();
+	void	resize();
+
+	void	mouseDown( MouseEvent event );	
+	void	mouseDrag( MouseEvent event );
+
+	bool	isInitialized() const { return (mDiffuseMap && mSpecularMap && mNormalMap 
+												&& mShader && mLightLantern && mLightAmbient && mMesh); }
+
+private:
+	void			delayedSetup();
+	TriMesh			createMesh(const fs::path& mshFile, const fs::path& objFile);
+	gl::VboMeshRef	createDebugMesh(const TriMesh& mesh);
+
+private:
+	Matrix44f			mMeshTransform;
+	AxisAlignedBox3f	mMeshBounds;
+
+	CameraPersp			mCamera;
+	MayaCamUI			mMayaCamera;
+
+	gl::Light*			mLightLantern;
+	gl::Light*			mLightAmbient;
+
+	gl::TextureRef		mLoadingMap;
+	gl::TextureRef		mDiffuseMap;
+	gl::TextureRef		mSpecularMap;
+	gl::TextureRef		mNormalMap;
+
+	gl::GlslProg		mShader;
+	gl::VboMeshRef		mMesh;
+	gl::VboMeshRef		mMeshDebug;
+
+	bool				bAutoRotate;
+	float				fAutoRotateAngle;
+
+	bool				bEnableDiffuseMap;
+	bool				bEnableSpecularMap;
+	bool				bEnableNormalMap;
+
+	bool				bShowNormalsAndTangents;
+	bool				bShowNormals;
+
+	float				fTime;
+
+	params::InterfaceGlRef	mParams;
+};
+
+void DeferredRenderingApp::prepareSettings(Settings* settings)
+{
+	settings->setWindowSize( 1024, 768 );
+	settings->setTitle( "Normal Mapping Demo" );
+}
+
+void DeferredRenderingApp::setup()
+{	
+	// load our "loading" message
+	mLoadingMap  = gl::Texture::create( loadImage( loadAsset("loading.png") ) );
+
+	// create a parameter window, so we can toggle stuff
+	mParams = params::InterfaceGl::create( getWindow(), "Normal Mapping Demo", Vec2i(300, 200) );
+	mParams->addParam( "Auto Rotate Model", &bAutoRotate );
+	mParams->addSeparator();
+	mParams->addParam( "Show Normals & Tangents", &bShowNormalsAndTangents );
+	mParams->addParam( "Show Normals", &bShowNormals );
+	mParams->addSeparator();
+	mParams->addParam( "Enable Diffuse Map", &bEnableDiffuseMap );
+	mParams->addParam( "Enable Specular Map", &bEnableSpecularMap );
+	mParams->addParam( "Enable Normal Map", &bEnableNormalMap );
+
+	// setup camera and lights
+	mCamera.setEyePoint( Vec3f( 0.2f, 0.4f, 1.8f ) );
+	mCamera.setCenterOfInterestPoint( Vec3f(0.0f, 0.5f, 0.0f) );
+	mCamera.setNearClip( 0.01f );
+	mCamera.setFarClip( 100.0f );
+
+	mLightLantern = new gl::Light(gl::Light::DIRECTIONAL, 0);
+	mLightLantern->setAmbient( Color(0.0f, 0.0f, 0.1f) );
+	mLightLantern->setDiffuse( Color(0.9f, 0.6f, 0.3f) );
+	mLightLantern->setSpecular( Color(0.9f, 0.6f, 0.3f) );
+
+	mLightAmbient = new gl::Light(gl::Light::DIRECTIONAL, 1);
+	mLightAmbient->setAmbient( Color(0.0f, 0.0f, 0.0f) );
+	mLightAmbient->setDiffuse( Color(0.2f, 0.6f, 1.0f) );
+	mLightAmbient->setSpecular( Color(0.2f, 0.2f, 0.2f) );
+
+	// default settings
+	mMeshBounds = AxisAlignedBox3f( Vec3f::zero(), Vec3f::one() );
+
+	bAutoRotate = true;
+	fAutoRotateAngle = 0.0f;
+
+	bEnableDiffuseMap = true;
+	bEnableSpecularMap = true;
+	bEnableNormalMap = true;
+
+	bShowNormalsAndTangents = false;
+	bShowNormals = false;
+
+	// keep track of time
+	fTime = (float) getElapsedSeconds();
+}
+
+void DeferredRenderingApp::delayedSetup()
+{
+	if( isInitialized() ) return;
+
+	// load textures and shaders
+	try {
+		mDiffuseMap = gl::Texture::create( loadImage( loadAsset("leprechaun_diffuse.jpg") ) );
+		mSpecularMap = gl::Texture::create( loadImage( loadAsset("leprechaun_specular.jpg") ) );
+		mNormalMap = gl::Texture::create( loadImage( loadAsset("leprechaun_normal.jpg") ) );
+
+		mShader = gl::GlslProg( loadAsset("normal_mapping_vert.glsl"), loadAsset("normal_mapping_frag.glsl") );
+	}
+	catch( const std::exception& e ) {
+		console() << "Error loading asset: " << e.what() << std::endl;
+	}
+
+	// load mesh file and create missing data (normals, tangents) if necessary
+	try {
+		fs::path objFile = getAssetPath("") / "leprechaun.obj";
+		fs::path mshFile = getAssetPath("") / "leprechaun.msh";
+		TriMesh mesh = createMesh( mshFile, objFile );
+
+		mMesh = gl::VboMesh::create( mesh );
+		mMeshBounds = mesh.calcBoundingBox();
+
+		mMeshDebug = createDebugMesh(mesh);
+	}
+	catch( const std::exception& e ) {
+		console() << "Error loading asset: " << e.what() << std::endl;
+	}
+}
+
+void DeferredRenderingApp::shutdown()
+{
+	if(mLightAmbient) delete mLightAmbient;
+	if(mLightLantern) delete mLightLantern;
+
+	mLightAmbient = mLightLantern = NULL;
+}
+
+void DeferredRenderingApp::update()
+{
+	// keep track of time
+	float fElapsed = (float) getElapsedSeconds() - fTime;
+	fTime += fElapsed;
+	
+	// because loading the model and shaders might take a while,
+	// we make sure our window is visible and cleared before calling 'delayedSetup()'
+	if( !isInitialized() && getElapsedFrames() > 5 ) {
+		delayedSetup();
+
+		// reset time after everything is setup
+		fTime = (float) getElapsedSeconds();
+	}
+	
+	// rotate the mesh
+	if(bAutoRotate) {
+		fAutoRotateAngle += (fElapsed * 0.2f);
+
+		mMeshTransform.setToIdentity();
+		mMeshTransform.rotate( Vec3f::yAxis(), fAutoRotateAngle );
+		mMeshTransform.scale( Vec3f::one() / mMeshBounds.getSize().y );
+	}
+}
+
+void DeferredRenderingApp::draw()
+{
+	gl::clear( Color(0.01f, 0.03f, 0.05f) ); 
+	gl::color( Color::white() );
+
+	if(!isInitialized())
+	{
+		// render our loading message while loading is in progress
+		Area centered = Area::proportionalFit( mLoadingMap->getBounds(), getWindowBounds(), true, false );
+
+		gl::enableAlphaBlending();
+		gl::draw( mLoadingMap, mLoadingMap->getBounds(), centered );
+		gl::disableAlphaBlending();
+	}
+	else
+	{
+		// get ready to draw in 3D
+		gl::pushMatrices();
+		gl::setMatrices( mCamera );
+
+		gl::enableDepthRead();
+		gl::enableDepthWrite();
+
+		// bind textures
+		mDiffuseMap->enableAndBind();
+		mSpecularMap->bind(1);
+		mNormalMap->bind(2);
+
+		// bind our normal mapping shader
+		mShader.bind();
+		mShader.uniform( "uDiffuseMap", 0 );
+		mShader.uniform( "uSpecularMap", 1 );
+		mShader.uniform( "uNormalMap", 2 );
+		mShader.uniform( "bShowNormals", bShowNormals );
+		mShader.uniform( "bUseDiffuseMap", bEnableDiffuseMap );
+		mShader.uniform( "bUseSpecularMap", bEnableSpecularMap );
+		mShader.uniform( "bUseNormalMap", bEnableNormalMap );
+
+		// enable our lights
+		mLightLantern->enable();
+		mLightAmbient->enable();
+
+		Vec3f lanternPositionOS = Vec3f(12.5f, 30.0f, 12.5f);
+		Vec3f lanternPositionWS = mMeshTransform.transformPointAffine( lanternPositionOS );
+		mLightLantern->lookAt( lanternPositionWS, Vec3f(0.0f, 0.5f, 0.0f) );
+		
+		mLightAmbient->lookAt( mCamera.getEyePoint(), mCamera.getCenterOfInterestPoint() );
+	
+		// render our model
+		gl::pushModelView();
+		gl::multModelView( mMeshTransform );
+		gl::draw( mMesh );
+		gl::popModelView();
+
+		// disable our lights
+		mLightAmbient->disable();
+		mLightLantern->disable();
+
+		// disable our shader
+		mShader.unbind();
+
+		// unbind textures
+		gl::disable( mDiffuseMap->getTarget() );
+	
+		// render normals, tangents and bitangents if necessary
+		if(bShowNormalsAndTangents) {
+			gl::pushModelView();
+			gl::multModelView( mMeshTransform );
+			gl::draw( mMeshDebug );
+			gl::popModelView();
+		}
+
+		// get ready to render in 2D again
+		gl::disableDepthWrite();
+		gl::disableDepthRead();
+
+		gl::popMatrices();
+
+		// render our parameter window
+		if(mParams)
+			mParams->draw();
+	}
+}
+
+void DeferredRenderingApp::resize()
+{
+	mCamera.setAspectRatio( getWindowAspectRatio() );
+}
+
+void DeferredRenderingApp::mouseDown( MouseEvent event )
+{
+	mMayaCamera.setCurrentCam( mCamera );
+	mMayaCamera.mouseDown( event.getPos() );
+}
+
+void DeferredRenderingApp::mouseDrag( MouseEvent event )
+{
+	mMayaCamera.mouseDrag( event.getPos(), event.isLeftDown(), event.isMiddleDown(), event.isRightDown() );
+	mCamera = mMayaCamera.getCamera();
+}
+
+TriMesh DeferredRenderingApp::createMesh(const fs::path& mshFile, const fs::path& objFile)
+{	
+	TriMesh mesh;	
+	Timer	timer;
+
+	// try to load the msh file (fast), or create it from the obj file (slow)
+	timer.start();
+	if(fs::exists(mshFile))
+		mesh.read( loadFile(mshFile) );
+	else 
+		ObjLoader( loadFile(objFile) ).load(&mesh); 
+	console() << "Loading the mesh took " << timer.getSeconds() << " seconds." << std::endl;
+
+	// create normals and tangents if necessary
+	bool hasChanged = !fs::exists(mshFile);
+
+	// if the mesh does not have normals, calculate them on-the-fly
+	if(!mesh.hasNormals()) {
+		timer.start();
+		mesh.recalculateNormals(); 
+		hasChanged = true;
+		console() << "Calculating " << mesh.getNumVertices() << " normals took " << timer.getSeconds() << " seconds." << std::endl;
+	}
+
+	// if the mesh does not have tangents, calculate them on-the-fly
+	//  (note: your model needs to have normals and texture coordinates for this to work)
+	if(!mesh.hasTangents()) {
+		timer.start();
+		mesh.recalculateTangents(); 
+		hasChanged = true;
+		console() << "Calculating " << mesh.getNumVertices() << " tangents took " << timer.getSeconds() << " seconds." << std::endl;
+	}
+
+	// export msh file if necessary
+	if(hasChanged) {
+		timer.start();
+		mesh.write( writeFile(mshFile) );
+		console() << "Saving the mesh took " << timer.getSeconds() << " seconds." << std::endl;
+	}
+
+	return mesh;
+}
+
+gl::VboMeshRef DeferredRenderingApp::createDebugMesh(const TriMesh& mesh)
+{
+	// create a debug mesh, showing normals, tangents and bitangents
+	size_t numVertices = mesh.getNumVertices();
+
+	std::vector<Vec3f>		vertices;	vertices.reserve( numVertices * 4 );
+	std::vector<Color>		colors;		colors.reserve( numVertices * 4 );
+	std::vector<uint32_t>	indices;	indices.reserve( numVertices * 6 );
+
+	for(size_t i=0;i<numVertices;++i) {
+		uint32_t idx = vertices.size();
+
+		vertices.push_back( mesh.getVertices()[i] );
+		vertices.push_back( mesh.getVertices()[i] + mesh.getTangents()[i] );
+		vertices.push_back( mesh.getVertices()[i] + mesh.getNormals()[i].cross(mesh.getTangents()[i]) );
+		vertices.push_back( mesh.getVertices()[i] + mesh.getNormals()[i] );
+
+		colors.push_back( Color(0, 0, 0) );	// base vertices black
+		colors.push_back( Color(1, 0, 0) );	// tangents (along u-coordinate) red
+		colors.push_back( Color(0, 1, 0) ); // bitangents (along v-coordinate) green
+		colors.push_back( Color(0, 0, 1) ); // normals blue
+
+		indices.push_back( idx );
+		indices.push_back( idx + 1 );
+		indices.push_back( idx );
+		indices.push_back( idx + 2 );
+		indices.push_back( idx );
+		indices.push_back( idx + 3 );
+	}
+
+	gl::VboMesh::Layout layout;
+	layout.setStaticPositions();
+	layout.setStaticColorsRGB();
+	layout.setStaticIndices();
+	
+	gl::VboMeshRef result = gl::VboMesh::create( numVertices * 4, numVertices * 6, layout, GL_LINES );
+	result->bufferPositions( vertices );
+	result->bufferColorsRGB( colors );
+	result->bufferIndices( indices );
+
+	return result;
+}
+
+CINDER_APP_NATIVE( DeferredRenderingApp, RendererGl )
